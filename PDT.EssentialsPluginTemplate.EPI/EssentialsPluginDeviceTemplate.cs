3bc3182e2a1fbef053e241b930126c3963554e5e
Jonathan Arndt <jarndt@pepperdash.com>
2020-11-05 16:04:43 -0800

    Initial commit

---
+++ PDT.EssentialsPluginTemplate.EPI/EssentialsPluginDeviceTemplate.cs
@@ -0,0 +1,57 @@
+﻿// For Basic SIMPL# Classes
+// For Basic SIMPL#Pro classes
+using Crestron.SimplSharpPro.DeviceSupport;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Bridges;
+using PepperDash.Core;
+
+namespace EssentialsPluginTemplateEPI 
+{
+	public class EssentialsPluginDeviceTemplate : EssentialsBridgeableDevice
+	{
+	    private EssentialsPluginConfigObjectTemplate _config;
+
+		public EssentialsPluginDeviceTemplate(string key, string name, EssentialsPluginConfigObjectTemplate config)
+			: base(key, name)
+		{
+            Debug.Console(0, this, "Constructing new EssentialsPluginDeviceTemplate instance");
+		    _config = config;
+		}
+
+	    #region Overrides of EssentialsBridgeableDevice
+
+	    public override void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApiAdvanced bridge)
+	    {
+	        var joinMap = new EssentialsPluginBridgeJoinMapTemplate(joinStart);
+
+	        // This adds the join map to the collection on the bridge
+	        if (bridge != null)
+	        {
+	            bridge.AddJoinMap(Key, joinMap);
+	        }
+
+	        var customJoins = JoinMapHelper.TryGetJoinMapAdvancedForDevice(joinMapKey);
+
+	        if (customJoins != null)
+	        {
+	            joinMap.SetCustomJoinData(customJoins);
+	        }
+
+	        Debug.Console(1, "Linking to Trilist '{0}'", trilist.ID.ToString("X"));
+	        Debug.Console(0, "Linking to Bridge Type {0}", GetType().Name);
+
+
+	        trilist.OnlineStatusChange += (o, a) =>
+	        {
+	            if (a.DeviceOnLine)
+	            {
+	                trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
+	            }
+	        };
+	    }
+
+
+	    #endregion
+	}
+}
+
