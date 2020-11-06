21c66e95c0973381d92c9f6f6c2762a8fac9446f
Jonathan Arndt <jarndt@pepperdash.com>
2020-11-05 23:21:53 -0800

    Updated folder paths

---
+++ EpiSwitcherPureLink/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("EssentialsPluginTemplateEpi")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialsPluginTemplateEpi")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-buildType-build#")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildType-build#")]
+
