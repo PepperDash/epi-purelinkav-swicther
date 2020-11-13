11e508d6bcd40f02f4c6ee0418b3e656f1a41a69
Jonathan Arndt <jarndt@pepperdash.com>
2020-11-13 08:37:08 -0800

    Updated Device and Bridge

--- EpiSwitcherPureLink/PureLinkBridgeJoinMap.cs
+++ EpiSwitcherPureLink/PureLinkBridgeJoinMap.cs
@@ -1,20 +1,126 @@
 ﻿using PepperDash.Essentials.Core;
+using DmChassisControllerJoinMap = PepperDash.Essentials.Core.Bridges.DmChassisControllerJoinMap;
 
 namespace EpiSwitcherPureLink
 {
 	public class PureLinkBridgeJoinMap : JoinMapBaseAdvanced
 	{
-	    public JoinDataComplete DeviceName = new JoinDataComplete(new JoinData {JoinNumber = 1, JoinSpan = 1},
-	        new JoinMetadata
-	        {
-	            Label = "Device Name",
-	            JoinCapabilities = eJoinCapabilities.ToSIMPL,
-	            JoinType = eJoinType.Serial
-	        });
+        public DmChassisControllerJoinMap PureLinkBaseJoinMap { get; set; }
 
-		public PureLinkBridgeJoinMap(uint joinStart) 
-            :base(joinStart)
+        // DmChassisControllerJoinMap is used as base, all join data below is an 
+        // extension of what doesn't exist in the DmChassisControllerJoinMap
+        [JoinName("Name")]
+        public JoinDataComplete DeviceName = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 1,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Device Name",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Serial
+            });
+
+        [JoinName("Connect")]
+        public JoinDataComplete Connect = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 1,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Connect",
+                JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                JoinType = eJoinType.Digital
+            });
+
+        [JoinName("Status")]
+        public JoinDataComplete Status = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 1,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Status",
+                JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                JoinType = eJoinType.Analog
+            });
+
+        //[JoinName("ErrorMessage")]
+        //public JoinDataComplete ErrorMessage = new JoinDataComplete(
+        //    new JoinData
+        //    {
+        //        JoinNumber = 2,
+        //        JoinSpan = 1
+        //    },
+        //    new JoinMetadata
+        //    {
+        //        Description = "Error Message",
+        //        JoinCapabilities = eJoinCapabilities.ToSIMPL,
+        //        JoinType = eJoinType.Serial
+        //    });
+
+        [JoinName("Poll")]
+        public JoinDataComplete Poll = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 6,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Poll",
+                JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                JoinType = eJoinType.Digital
+            });
+
+        [JoinName("PollLabels")]
+        public JoinDataComplete PollLabels = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 7,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Poll Labels",
+                JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                JoinType = eJoinType.Digital
+            });
+
+        [JoinName("PollCrosspoints")]
+        public JoinDataComplete PollCrosspoints = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 8,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Poll Crosspoints",
+                JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                JoinType = eJoinType.Digital
+            });
+
+        public PureLinkBridgeJoinMap(uint joinStart)
+            : base(joinStart, typeof(PureLinkBridgeJoinMap))
 		{
+			// the code below merges the EvertzQuartBaseJoinMap (referencing the DmChassisControllerJoinMap) to the join maps
+			PureLinkBaseJoinMap = new DmChassisControllerJoinMap(joinStart);		// as of 1.5.6
+			//EvertzQuartzBaseJoinMap = new DmChassisControllerJoinMap(joinStart, typeof(DmChassisControllerJoinMap));	// as of 1.5.8
+
+            foreach (var join in PureLinkBaseJoinMap.Joins)
+			{
+				if (Joins.ContainsKey(join.Key))
+					Joins[join.Key] = join.Value;
+				else 
+					Joins.Add(join.Key, join.Value);
+			}
 		}
 	}
 }
--- EpiSwitcherPureLink/PureLinkDevice.cs
+++ EpiSwitcherPureLink/PureLinkDevice.cs
@@ -38,49 +38,447 @@ namespace EpiSwitcherPureLink
         private GenericCommunicationMonitor _commsMonitor;
         private CommunicationGather _commsGather;
 
+        private long PollTime = 30000; // 30s
+        private long WarningTimeout = 60000; // 60s
+        private long ErrorTimeout = 180000; // 180s
+
+        public bool Connect
+        {
+            get { return _comms.IsConnected; }
+            set
+            {
+                if (value == true)
+                {
+                    _comms.Connect();
+                    _commsMonitor.Start();
+                }
+                else
+                {
+                    _comms.Disconnect();
+                    _commsMonitor.Stop();
+                }
+            }
+        }
+
+        private Dictionary<uint, string> _sourceVideoNames;
+        private Dictionary<uint, string> _sourceAudioNames;
+
+        private Dictionary<uint, string> _destVideoNames;
+        private Dictionary<uint, string> _destAudioNames;
+
+        private bool _audioBreakawayEnabled;
+
+        /// <summary>
+        /// Audio breakaway enabled property
+        /// </summary>
+        public bool AudioBreakawayEnabled
+        {
+            get { return _audioBreakawayEnabled; }
+            set
+            {
+                if (value == _audioBreakawayEnabled) return;
+                _audioBreakawayEnabled = value;
+                AudioBreakawayEnabledFeedback.FireUpdate();
+            }
+        }
+        /// <summary>
+        /// Audio breakaway enabled feedback
+        /// </summary>
+        public BoolFeedback AudioBreakawayEnabledFeedback { get; private set; }
+
+        public BoolFeedback ConnectFeedback { get; private set; }
+        public BoolFeedback OnlineFeedback { get; private set; }
+        public IntFeedback StatusFeedback { get; private set; }
+
+        public Dictionary<uint, StringFeedback> SourceVideoNameFeedbacks { get; private set; }
+        public Dictionary<uint, StringFeedback> SourceAudioNameFeedbacks { get; private set; }
+
+        public Dictionary<uint, StringFeedback> DestVideoNameFeedbacks { get; private set; }
+        public Dictionary<uint, StringFeedback> DestAudioNameFeedbacks { get; private set; }
+
+        public Dictionary<uint, IntFeedback> DestCurrentVideoInputValueFeedbacks { get; private set; }
+        public Dictionary<uint, IntFeedback> DestCurrentAudioInputValueFeedbacks { get; private set; }
+
+        public Dictionary<uint, StringFeedback> DestCurrentVideoInputNameFeedbacks { get; private set; }
+        public Dictionary<uint, StringFeedback> DestCurrentAudioInputNameFeedbacks { get; private set; }
+
 	    private PureLinkConfig _config;
 
-		public PureLinkDevice(string key, string name, PureLinkConfig config)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="name"></param>
+        /// <param name="config"></param>
+        public PureLinkDevice(string key, string name, PureLinkConfig config, IBasicCommunication comms)
 			: base(key, name)
 		{
-            Debug.Console(0, this, "Constructing new EssentialsPluginDeviceTemplate instance");
-		    _config = config;
+            Debug.Console(0, this, "Constructing new Pure Link plugin instance");
+
+            _sourceVideoNames = new Dictionary<uint, string>();
+            _sourceAudioNames = new Dictionary<uint, string>();
+
+            _destVideoNames = new Dictionary<uint, string>();
+            _destAudioNames = new Dictionary<uint, string>();
+
+            _destCurrentVideoInputValue = new Dictionary<uint, uint>();
+            _destCurrentAudioInputValue = new Dictionary<uint, uint>();
+
+            ConnectFeedback = new BoolFeedback(() => Connect);
+            OnlineFeedback = new BoolFeedback(() => _commsMonitor.IsOnline);
+            StatusFeedback = new IntFeedback(() => (int)_commsMonitor.Status);
+
+            AudioBreakawayEnabledFeedback = new BoolFeedback(() => AudioBreakawayEnabled);
+
+            SourceVideoNameFeedbacks = new Dictionary<uint, StringFeedback>();
+            SourceAudioNameFeedbacks = new Dictionary<uint, StringFeedback>();
+
+            DestVideoNameFeedbacks = new Dictionary<uint, StringFeedback>();
+            DestAudioNameFeedbacks = new Dictionary<uint, StringFeedback>();
+
+            DestCurrentVideoInputValueFeedbacks = new Dictionary<uint, IntFeedback>();
+            DestCurrentAudioInputValueFeedbacks = new Dictionary<uint, IntFeedback>();
+
+            DestCurrentVideoInputNameFeedbacks = new Dictionary<uint, StringFeedback>();
+            DestCurrentAudioInputNameFeedbacks = new Dictionary<uint, StringFeedback>();
+
+            if (config.PollTime > 0 && config.PollTime != PollTime)
+                PollTime = config.PollTime;
+
+            if (config.WarningTimeout > 0 && config.WarningTimeout != WarningTimeout)
+                WarningTimeout = config.WarningTimeout;
+
+            if (config.ErrorTimeout > 0 && config.ErrorTimeout != ErrorTimeout)
+                ErrorTimeout = config.ErrorTimeout;
+
+            _comms = comms;
+            _commsGather = new CommunicationGather(_comms, Delimter);
+            if (_commsGather == null)
+            {
+                Debug.Console(2, this, "_commsGather is null");
+            }
+            _commsMonitor = new GenericCommunicationMonitor(this, _comms, PollTime, WarningTimeout, ErrorTimeout, Poll);
+
+            var socket = _comms as ISocketStatus;
+            if (socket != null)
+            {
+                socket.ConnectionChange += (sender, args) =>
+                {
+                    Debug.Console(2, this, args.Client.ClientStatus.ToString());
+                    if (ConnectFeedback != null) ConnectFeedback.FireUpdate();
+                    if (StatusFeedback != null) StatusFeedback.FireUpdate();
+                };
+            }
+
+            AddPostActivationAction(() => _commsGather.LineReceived += HandleLineReceived);
+            AddPostActivationAction(() => Connect = true);
+
+            AddPostActivationAction(() => InitializeInputs(config.Inputs));
+            AddPostActivationAction(() => InitializeOutputs(config.Outputs));
 		}
 
 	    #region Overrides of EssentialsBridgeableDevice
 
 	    public override void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApiAdvanced bridge)
 	    {
-	        var joinMap = new PureLinkBridgeJoinMap(joinStart);
+            			var joinMap = new PureLinkBridgeJoinMap(joinStart);
 
-	        // This adds the join map to the collection on the bridge
-	        if (bridge != null)
-	        {
-	            bridge.AddJoinMap(Key, joinMap);
-	        }
+			// This adds the join map to the collection on the bridge
+			if (bridge != null)
+			{
+				bridge.AddJoinMap(Key, joinMap);
+			}
 
-	        var customJoins = JoinMapHelper.TryGetJoinMapAdvancedForDevice(joinMapKey);
+			var customJoins = JoinMapHelper.TryGetJoinMapAdvancedForDevice(joinMapKey);
+			if (customJoins != null)
+			{
+				joinMap.SetCustomJoinData(customJoins);
+			}
 
-	        if (customJoins != null)
-	        {
-	            joinMap.SetCustomJoinData(customJoins);
-	        }
+			Debug.Console(1, "Linking to Trilist '{0}'", trilist.ID.ToString("X"));
+			Debug.Console(0, "Linking to Bridge Type {0}", GetType().Name);
 
-	        Debug.Console(1, "Linking to Trilist '{0}'", trilist.ID.ToString("X"));
-	        Debug.Console(0, "Linking to Bridge Type {0}", GetType().Name);
+			// link joins to bridge
+			trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
 
+			ConnectFeedback.LinkInputSig(trilist.BooleanInput[joinMap.Connect.JoinNumber]);
+			StatusFeedback.LinkInputSig(trilist.UShortInput[joinMap.Status.JoinNumber]);
+			OnlineFeedback.LinkInputSig(trilist.BooleanInput[joinMap.PureLinkBaseJoinMap.IsOnline.JoinNumber]);
 
-	        trilist.OnlineStatusChange += (o, a) =>
-	        {
-	            if (a.DeviceOnLine)
-	            {
-	                trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
-	            }
-	        };
+			//ErrorMessageFeedback.LinkInputSig(trilist.StringInput[joinMap.ErrorMessage.JoinNumber]);
+
+			trilist.SetSigTrueAction(joinMap.Poll.JoinNumber, Poll);
+			trilist.SetSigTrueAction(joinMap.PollCrosspoints.JoinNumber, PollCrosspoints);
+			trilist.SetSigTrueAction(joinMap.PollLabels.JoinNumber, PollNames);
+
+			trilist.SetBoolSigAction(joinMap.PureLinkBaseJoinMap.EnableAudioBreakaway.JoinNumber, SetAudioBreakawayState);
+			AudioBreakawayEnabledFeedback.LinkInputSig(
+				trilist.BooleanInput[joinMap.PureLinkBaseJoinMap.EnableAudioBreakaway.JoinNumber]);
+
+			foreach (var item in SourceVideoNameFeedbacks)
+			{
+				var join = item.Key + joinMap.PureLinkBaseJoinMap.InputVideoNames.JoinNumber - 1;
+				var feedback = item.Value;
+				if (feedback == null) continue;
+				feedback.LinkInputSig(trilist.StringInput[join]);
+			}
+
+			foreach (var item in SourceAudioNameFeedbacks)
+			{
+				var join = item.Key + joinMap.PureLinkBaseJoinMap.InputAudioNames.JoinNumber - 1;
+				;
+				var feedback = item.Value;
+				if (feedback == null) continue;
+				feedback.LinkInputSig(trilist.StringInput[join]);
+			}
+
+			foreach (var item in DestVideoNameFeedbacks)
+			{
+				var join = item.Key + joinMap.PureLinkBaseJoinMap.OutputVideoNames.JoinNumber - 1;
+				var feedback = item.Value;
+				if (feedback == null) continue;
+				feedback.LinkInputSig(trilist.StringInput[join]);
+			}
+
+			foreach (var item in DestAudioNameFeedbacks)
+			{
+				var join = item.Key + joinMap.PureLinkBaseJoinMap.OutputAudioNames.JoinNumber - 1;
+				var feedback = item.Value;
+				if (feedback == null) continue;
+				feedback.LinkInputSig(trilist.StringInput[join]);
+			}
+
+			foreach (var item in DestCurrentVideoInputValueFeedbacks)
+			{
+				var join = item.Key + joinMap.PureLinkBaseJoinMap.OutputVideo.JoinNumber - 1;
+
+				var output = item.Key;
+				trilist.SetUShortSigAction(join, input => ExecuteSwitch(input, output, eRoutingSignalType.Video));
+
+				var feedback = item.Value;
+				if (feedback == null) continue;
+				feedback.LinkInputSig(trilist.UShortInput[join]);
+			}
+
+			foreach (var item in DestCurrentAudioInputValueFeedbacks)
+			{
+				var join = item.Key + joinMap.PureLinkBaseJoinMap.OutputAudio.JoinNumber - 1;
+
+				var output = item.Key;
+				trilist.SetUShortSigAction(join, input => ExecuteSwitch(input, output, eRoutingSignalType.Audio));
+
+				var feedback = item.Value;
+				if (feedback == null) continue;
+				feedback.LinkInputSig(trilist.UShortInput[join]);
+			}
+
+			foreach (var item in DestCurrentVideoInputNameFeedbacks)
+			{
+				var join = item.Key + joinMap.PureLinkBaseJoinMap.OutputCurrentVideoInputNames.JoinNumber - 1;
+				var feedback = item.Value;
+				if (feedback == null) continue;
+				feedback.LinkInputSig(trilist.StringInput[join]);
+			}
+
+			foreach (var item in DestCurrentAudioInputNameFeedbacks)
+			{
+				var join = item.Key + joinMap.PureLinkBaseJoinMap.OutputCurrentAudioInputNames.JoinNumber - 1;
+				var feedback = item.Value;
+				feedback.LinkInputSig(trilist.StringInput[join]);
+			}
+
+			UpdateFeedbacks();
+
+			trilist.OnlineStatusChange += (o, a) =>
+			{
+				if (a.DeviceOnLine)
+				{
+					trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
+
+					UpdateFeedbacks();
+				}
+			};
 	    }
-
-
 	    #endregion
+
+        #region UpdateRouteFeedbacks
+
+        private void UpdateRouteFeedback(uint input, uint output, string level)
+        {
+            Debug.Console(2, this, "UpdateRouteFeedback({0}, {1}, {2})", input, output, level);
+
+            if (level == VideoLevel)
+                UpdateRouteVideoFeedback(input, output);
+
+            else if (level == AudioLevel)
+                UpdateRouteAudioFeedback(input, output);
+
+            else if (level == UsbLevel)
+                UpdateRouteUsbFeedback(input, output);
+
+            else
+                Debug.Console(2, this, "UpdateRouteFeedback: undefined route type {0}", level);
+        }
+
+        private void UpdateRouteVideoFeedback(uint input, uint output)
+        {
+            Debug.Console(2, this, "UpdateRouteVideoFeedback({0}, {1})", input, output);
+
+            uint inputKey = 0;
+            uint outputKey = 0;
+            var inputName = "Unknown";
+
+            IntFeedback valueFeedback;
+            StringFeedback nameFeedback;
+
+            // update route value feedback
+            if (DestCurrentVideoInputValueFeedbacks.TryGetValue(outputKey, out valueFeedback))
+                if (valueFeedback != null) valueFeedback.FireUpdate();
+
+            // get the input name
+            _sourceVideoNames.TryGetValue(inputKey, out inputName);
+
+            // update route name feedback
+            if (!DestCurrentVideoInputNameFeedbacks.TryGetValue(outputKey, out nameFeedback))
+            {
+                nameFeedback = new StringFeedback(() => inputName);
+                DestCurrentVideoInputNameFeedbacks.Add(outputKey, nameFeedback);
+            }
+            else
+            {
+                DestCurrentVideoInputNameFeedbacks[outputKey] = nameFeedback;
+            }
+            nameFeedback.FireUpdate();
+        }
+
+        private void UpdateRouteAudioFeedback(uint input, uint output)
+        {
+            Debug.Console(2, this, "UpdateRouteAudioFeedback({0}, {1})", input, output);
+
+            uint inputKey = 0;
+            uint outputKey = 0;
+            var inputName = "Unknown";
+
+            IntFeedback valueFeedback;
+            StringFeedback nameFeedback;
+
+            // update route value feedback
+            if (DestCurrentAudioInputValueFeedbacks.TryGetValue(outputKey, out valueFeedback))
+                if (valueFeedback != null) valueFeedback.FireUpdate();
+
+            // get the input name
+            _sourceAudioNames.TryGetValue(inputKey, out inputName);
+
+            // update route name feedback
+            if (!DestCurrentAudioInputNameFeedbacks.TryGetValue(outputKey, out nameFeedback))
+            {
+                nameFeedback = new StringFeedback(() => inputName);
+                DestCurrentAudioInputNameFeedbacks.Add(outputKey, nameFeedback);
+            }
+            else
+            {
+                DestCurrentAudioInputNameFeedbacks[outputKey] = nameFeedback;
+            }
+            nameFeedback.FireUpdate();
+        }
+        #endregion
+
+        #region UpdateSourceNameFeedback & UpdateDestinationNameFeedback
+
+        private void UpdateSourceNameFeedback(uint index, string name)
+        {
+            Debug.Console(2, this, "UpdateSourceNameFeedback({0}, {1})", index, name);
+
+            // TODO [WIP] Complete method
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+
+            var feedback = new StringFeedback(() => name);
+
+            var key = _sourceQvIndexes.FirstOrDefault(k => k.Value == index).Key;
+            if (key != 0)
+            {
+                _sourceVideoNames[key] = name;
+                if (SourceVideoNameFeedbacks.ContainsKey(key))
+                    SourceVideoNameFeedbacks[key] = feedback;
+                else
+                    SourceVideoNameFeedbacks.Add(key, feedback);
+
+                feedback.FireUpdate();
+                return;
+            }
+
+            key = _sourceQaIndexes.FirstOrDefault(k => k.Value == index).Key;
+            if (key != 0)
+            {
+                _sourceAudioNames[key] = name;
+                if (SourceAudioNameFeedbacks.ContainsKey(key))
+                    SourceAudioNameFeedbacks[key] = feedback;
+                else
+                    SourceAudioNameFeedbacks.Add(key, feedback);
+
+                feedback.FireUpdate();
+                return;
+            }
+
+            key = _sourceQuIndexes.FirstOrDefault(k => k.Value == index).Key;
+            _sourceUsbNames[key] = name;
+            if (SourceUsbNameFeedbacks.ContainsKey(key))
+                SourceUsbNameFeedbacks[key] = feedback;
+            else
+                SourceUsbNameFeedbacks.Add(key, feedback);
+        }
+
+        private void UpdateDestinationNameFeedback(uint index, string name)
+        {
+            Debug.Console(2, this, "UpdateDestinationNameFeedback({0}, {1})", index, name);
+
+            // TODO [WIP] Complete method
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+
+            var feedback = new StringFeedback(() => name);
+
+            var key = _destQvIndexes.FirstOrDefault(k => k.Value == index).Key;
+            if (key != 0)
+            {
+                _destVideoNames[key] = name;
+                if (DestVideoNameFeedbacks.ContainsKey(key))
+                    DestVideoNameFeedbacks[key] = feedback;
+                else
+                    DestVideoNameFeedbacks.Add(key, feedback);
+
+                feedback.FireUpdate();
+                return;
+            }
+
+            key = _destQaIndexes.FirstOrDefault(k => k.Value == index).Key;
+            if (key != 0)
+            {
+                if (DestAudioNameFeedbacks.ContainsKey(key))
+                    DestAudioNameFeedbacks[key] = feedback;
+                else
+                    DestAudioNameFeedbacks.Add(key, feedback);
+
+                feedback.FireUpdate();
+                return;
+            }
+
+            key = _destQuIndexes.FirstOrDefault(k => k.Value == index).Key;
+            _destUsbNames[key] = name;
+            if (DestUsbNameFeedbacks.ContainsKey(key))
+                DestUsbNameFeedbacks[key] = feedback;
+            else
+                DestUsbNameFeedbacks.Add(key, feedback);
+
+            feedback.FireUpdate();
+        }
+
+        #endregion
 
         #region Polls
 
@@ -193,7 +591,6 @@ namespace EpiSwitcherPureLink
         }
 
         #endregion
-
 
         #region SetBreakawayStates
 
